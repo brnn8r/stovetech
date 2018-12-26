d342d33cf43767efdb403deec25414251fbac2c0
Stephen Christopher Adams <vengeanz@gmail.com>
2018-12-26 21:06:52 +1300

    Removed key pair and backup file

--- infra/main.tf
+++ infra/main.tf
@@ -19,7 +19,6 @@ resource "aws_instance" "test_instance" {
   instance_type = "t2.micro"
   iam_instance_profile = "${aws_iam_instance_profile.ec2_instance_profile.name}"
   security_groups = ["${aws_security_group.web_dmz_security_group.name}"]
-  key_name = "StovesEc2KeyPair"
   user_data = "sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm"
 
   tags {
